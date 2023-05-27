11abb41b47593de5d1d9881633cf6845880b2904
Crowdbotics <team@crowdbotics.com>
2023-05-27 07:57:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fastway-41036",
+  url: "https://fastway-41036.botics.co",
+  api: "https://fastway-41036.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
